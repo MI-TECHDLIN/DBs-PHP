ac5edaa80405140e4687bae3b441a654c44eb7c0
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-03-05 21:33:18 +0100

    documentation,userchecker updated

--- connect.php
+++ connect.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * before you touch:
+ * edit the dabatase name at default = demoregistration
+ * edit the table at default = usertable
+ * i implememted a condition to check if this (firstname,email) is presents in my table
+ *  if true return a alert
+ * else continue the procces create a new table for that data
+ *  
+ */
+
+
 
 include 'db.php';
 // collection of data
@@ -10,14 +21,33 @@ $gender=$_POST['gender'];
 $age=$_POST['age'];
 
 // check for connection_error
-
+// this is where we start our query
 if ($connection->connect_error){
     echo 'Error while connectnig', $connection->error;}
-
-// this is where we start our query
 else{
+
+
+// CHECK IF USER ALREADY EXISTS
+$check = $connection->prepare("SELECT * FROM usertable WHERE email = ? AND firstname = ?");
+ /**
+     * Check if a user already exists with the same email and firstname in my database.
+     * This prevents duplicate registrations.
+     * Prepared statements are used to prevent SQL injection.
+     */
+$check->bind_param("ss", $email, $firstname);
+$check->execute();
+$result = $check->get_result();
+
+if ($result->num_rows > 0) {
+
+    echo "User with this email and firstname already exists.";
+
+}
+else{
+
 $sqrt=$connection->prepare("INSERT INTO usertable(firstname,lastname,email,password,gender,age) VALUES(?,?,?,?,?,?)");
 $sqrt->bind_param("sssssi",$firstname,$lastname,$email,$password,$gender,$age);
+
 if($sqrt->execute()){
     echo 'User ',$firstname, ' Connected Sucessful';
 }
@@ -27,4 +57,8 @@ else{
 $connection->close();
 $sqrt->close();
 } 
+}
 ?>
+
+
+
